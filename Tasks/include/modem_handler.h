7f74f1c8df65509c0aa11829c3e4b4a438253de4
niceLemonGuy <46706839+niceLemonGuy@users.noreply.github.com>
2019-01-16 23:03:00 +0300

    Update modem_handler.h

--- Tasks/include/modem_handler.h
+++ Tasks/include/modem_handler.h
@@ -21,8 +21,8 @@
 
 
 /* Defines -------------------------------------------------------------------*/
-#define MODEM_WAIT_TIME 				10	// Ожидание ответа от модема [сек]
-#define MODEM_MAX_LINK_ERR			5		// Допустимое кол-во ошибок связи
+#define MODEM_WAIT_TIME 	10 // Ожидание ответа от модема [сек]
+#define MODEM_MAX_LINK_ERR	5  // Допустимое кол-во ошибок связи
 
 
 /* Task ---------------------------------------------------------------------*/
@@ -37,17 +37,17 @@ extern OS_TID tid_Aux_Modem_Handler;	// ID задачи t_Aux_Modem_Handler
 
 /* Global Functions ---------------------------------------------------------*/
 bool gprs_write_msg(U8* buff, U16 len);	// Отправить сообщение по gprs
-bool gprs_recv_msg(U8* buff);						// Получить принятое по gprs сообщение 
-void msg_processed(void);								// Сообщение обработано
-void select_sim1(void);									// Работать с SIM1
-void select_sim2(void);									// Работать с SIM2
-void change_sim(void);									// Поменять SIM карту
+bool gprs_recv_msg(U8* buff);		// Получить принятое по gprs сообщение 
+void msg_processed(void);		// Сообщение обработано
+void select_sim1(void);			// Работать с SIM1
+void select_sim2(void);			// Работать с SIM2
+void change_sim(void);			// Поменять SIM карту
 
 
 /* Structs ------------------------------------------------------------------*/
 typedef struct _Modem_Stat_t
 {
-	U8 aux_on: 1;		// 0:Aux не запущен		1:Aux запущен
+	U8 aux_on: 1;	// 0:Aux не запущен	1:Aux запущен
 	U8 err_cnt: 3;	// Счётчик ошибок при работе с активной SIM картой
 }Modem_Stat_t;
 
